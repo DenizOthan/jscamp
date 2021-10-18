27b66218548a20657f05c2bc1c352505bc4c21fe
DenizOthan <deryadeniz453@gmail.com>
2021-10-19 00:42:56 +0300

    Proje Yüklendi

---
+++ src/app.js
@@ -0,0 +1,31 @@
+console.log("merhaba kodlama.io")
+
+let dolarBugun = 9.30
+let dolarDun =9.20
+{
+    let dolarDun =9.10
+}
+
+console.log(dolarDun)
+
+const eurDun = 11.2
+///eurDun =11
+console.log(eurDun)
+ 
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri 
+=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Kredi"]
+console.log("<ul>")
+
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
+
+
+
